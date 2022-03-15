af20ec344e62eb0093885d23324f69c25621271c
johnkuch <58474532+johnkuch@users.noreply.github.com>
2022-03-15 19:23:50 -0400

    Update create_sink_ddl.sql
    properties.server.bootstrap
    changed connector to 'connector' = 'kafka:  Local Kafka'

--- 201/ddos_attack/create_sink_ddl.sql
+++ 201/ddos_attack/create_sink_ddl.sql
@@ -1,11 +1,13 @@
+DROP TABLE `slowloris_attacks`;
 CREATE TABLE  `slowloris_attacks` (
   `alert` CHAR(4) NOT NULL,
+  `message` VARCHAR(2147483647) NULL,
   `source_ip` VARCHAR(2147483647),
   `window_start` TIMESTAMP(3) NOT NULL,
   `window_end` TIMESTAMP(3) NOT NULL,
   `count_connection_reset` BIGINT NOT NULL
 ) WITH (
-  'connector' = 'kafka',                    -- Specify what connector to use, for Kafka it must use 'kafka'.
+  'connector' = 'kafka:  Local Kafka',      -- Specify what connector to use, for Kafka it must use 'kafka'.
   'format' = 'json',                        -- Data format
-  'properties.bootstrap.servers' = 'kafka', -- Comma separated list of Kafka brokers.
   'topic' = 'slowloris'
+);
