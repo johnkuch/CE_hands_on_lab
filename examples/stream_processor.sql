344c0d875489af32f914225e8e2db71338450a02
johnkuch <58474532+johnkuch@users.noreply.github.com>
2022-03-15 19:30:24 -0400

    Update stream_processor.sql
    change table name to output_table match docs

--- examples/stream_processor.sql
+++ examples/stream_processor.sql
@@ -1,4 +1,4 @@
-INSERT INTO output
+INSERT INTO output_table
 SELECT window_start, window_end, order_status, COUNT(*) as order_count, SUM(amount) as total_amount
 FROM TABLE(
 TUMBLE(TABLE orders, DESCRIPTOR(order_time), INTERVAL '20' SECONDS))
